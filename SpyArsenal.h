1e0ee1352ffb74af6b9ee8344b34f1455b4e40e1
VIktoriya Cherevatenko <cherevatenkoviktoriya@gmail.com>
2026-05-01 23:28:48 +0300

    Do second task insert vector elements in another, SpyArsenal addMissionTasksCodes

--- SpyArsenal.h
+++ SpyArsenal.h
@@ -2,6 +2,7 @@
 // Created by newvi on 01.05.2026.
 //
 #pragma once
+#include <vector>
 #include <array>
 #include <iostream>
 #ifndef STLCONTAINERSLEARN_SPYARSENAL_H
@@ -22,6 +23,41 @@ public:
     std::array<char, 26> spyAlphabet{};
 
     int shiftKey = 3;
+
+
+    /////////
+    ///
+    std::vector<std::string> missionTasksCodes{
+        "3801297356287",
+        "3809523465",
+        "380667435421",
+        "380123456789",
+        "380987654321",
+        "380111111111",
+    };
+
+    template <typename T>
+    void addMissionTasksCodes(std::vector<T>& oldTaskCodes, std::vector<T>& toAddTaskCodes, int placementPosition) {
+        int count = 0; //number
+        auto it = oldTaskCodes.begin(); // pointer
+        while (it !=  oldTaskCodes.end()) {
+            if (count == placementPosition - 2) {
+                auto insertIt = oldTaskCodes.begin() + placementPosition;
+                for (int i = 0; i < toAddTaskCodes.size(); i++) {
+                    insertIt = oldTaskCodes.insert(insertIt, toAddTaskCodes[i]);
+                    ++insertIt;
+                }
+                break;
+            }
+            ++it;
+            ++count;
+        }
+
+        std::cout << "Success!" << std::endl;
+        for (int i = 0; i < oldTaskCodes.size(); i++) {
+            std::cout << oldTaskCodes[i] << ", " << std::endl;
+        }
+    };
 };
 
 
--- SpyController.cpp
+++ SpyController.cpp
@@ -10,6 +10,7 @@
 void SpyController::MakeAction() {
     std::cout << "Welcome back, dear spy! Choose what to do: " << std::endl;
     std::cout << "Encrypt the alphabet - git enc a" << std::endl;
+    std::cout << "Add new mission codes to system - git add mcodes" << std::endl;
 
     std::string spyInput;
     std::getline(std::cin, spyInput);
@@ -19,6 +20,33 @@ void SpyController::MakeAction() {
         std::string shiftKey;
         std::cin >> shiftKey;
         spyArsenal.encryptionBox->shiftAlphabetElements(spyArsenal.standardAlphabet, spyArsenal.spyAlphabet, std::stoi(shiftKey));
+
+    }
+    else if (spyInput == "git add mcodes") {
+        std::cout << "Please enter how many new codes: " << std::endl;
+        std::string newCodeCount;
+        std::cin >> newCodeCount;
+
+        std::cout << "Please enter a placement position of new codes: " << std::endl;
+        std::string placementPosition;
+        std::cin >> placementPosition;
+
+
+        std::vector<std::string> toAddTaskCodes{};
+        std::cout << "Please enter arrived toAddTaskCodes: " << std::endl;
+        for (int i = 0; i < std::stoi(newCodeCount); i++) {
+            std::string code;
+            std::cin >> code;
+            toAddTaskCodes.push_back(code);
+        }
+        std::cout << "System processing the data right now..." << std::endl;
+
+        std::cout << "Take a look what was before: " << std::endl;
+        for (const auto & missionTasksCode : spyArsenal.missionTasksCodes) {
+            std::cout << missionTasksCode << ", " << std::endl;
+        }
+
+        spyArsenal.addMissionTasksCodes(spyArsenal.missionTasksCodes, toAddTaskCodes, std::stoi(placementPosition));
 
     }
     else {
